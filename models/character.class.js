dec0c7a48b42735d2f5f4da6a4093f23b54c6571
Judith L. <judith.lenz@outlook.de>
2024-11-11 19:55:52 +0100

    imageCache

--- models/character.class.js
+++ models/character.class.js
@@ -4,7 +4,15 @@ class Character extends MovableObject {
   y = 220;
 
   constructor() {
-    super().loadImage("img/2_character_pepe/1_idle/idle/I-1.png");
+    super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages([
+      "img/2_character_pepe/2_walk/W-21.png",
+      "img/2_character_pepe/2_walk/W-22.png",
+      "img/2_character_pepe/2_walk/W-23.png",
+      "img/2_character_pepe/2_walk/W-24.png",
+      "img/2_character_pepe/2_walk/W-25.png",
+      "img/2_character_pepe/2_walk/W-26.png",
+    ]); //das ganze Array wird als Parameter übergeben
   }
 
   jump() {}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,15 +8,17 @@ class MovableObject {
 
   //loadImage('img/test.png);
   loadImage(path) {
-    this.img = new Image(); //this.img = document.getElementById ('image') <img id = "image" src>
+    this.img = new Image(); //this.img =dasselbe wie: document.getElementById ('image') <img id = "image" src>
     this.img.src = path;
   }
 
   loadImages(arr) {
+    // hier wurde als Parameter das ganze Array reingegeben. und dann iteriert man durch
     arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      imageCache[path] = path;
+      //path hat jedes mal den Pfad von einem Bild
+      let img = new Image(); //hier laden wir das Bild in das image Objekt.
+      img.src = path; // hier ist nur innerhalb dieser Funktion gültige Variable
+      this.imageCache[path] = path; //hier greifen wir auf die Variable von dem Objekt zu (s.o.)
     });
   }
 
