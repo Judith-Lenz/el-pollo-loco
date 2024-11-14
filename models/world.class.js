aea4a558444607e278d8e8faa8a3099b7ffe72b8
Judith L. <judith.lenz@outlook.de>
2024-11-14 15:32:55 +0100

    Level Objekt

--- models/world.class.js
+++ models/world.class.js
@@ -1,52 +1,9 @@
 class World {
   //an der x-Koordinate 0 werden 4 Grafiken eingefügt.
-  backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/air.png", -739),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -739),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -739),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -739),
-
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-
-    new BackgroundObject("img/5_background/layers/air.png", 739),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 739),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 739),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 739),
-
-    new BackgroundObject("img/5_background/layers/air.png", 739 * 2),
-    new BackgroundObject(
-      "img/5_background/layers/3_third_layer/1.png",
-      739 * 2
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/2_second_layer/1.png",
-      739 * 2
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/1_first_layer/1.png",
-      739 * 2
-    ),
-
-    new BackgroundObject("img/5_background/layers/air.png", 739 * 3),
-    new BackgroundObject(
-      "img/5_background/layers/3_third_layer/2.png",
-      739 * 3
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/2_second_layer/2.png",
-      739 * 3
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/1_first_layer/2.png",
-      739 * 3
-    ),
-  ];
+  backgroundObjects = level1.backgroundObjects;
   character = new Character(); //Variable character wurde Objekt namens Character zugewiesen
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()];
+  enemies = level1.enemies;
+  clouds = level1.clouds;
 
   canvas;
   ctx; //Variable context
