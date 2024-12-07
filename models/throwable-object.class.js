b09ffcd23937d9600bf331509e863de373520ac5
Judith L. <judith.lenz@outlook.de>
2024-12-07 18:12:43 +0100

    optimated bottle fly

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,6 +1,7 @@
 class ThrowableObject extends MovableObject {
   speedX = 20;
   speedY = 30;
+  acceleration = 1.0; // Individueller Wert für Gravitation nur für ThrowableObject
 
   constructor(x, y) {
     super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
@@ -12,10 +13,10 @@ class ThrowableObject extends MovableObject {
   }
 
   throw() {
-    this.speedY = 30; // Tempo nach oben
+    this.speedY = 15; // Tempo nach oben
     this.applyGravity(); // Gravitation hinzufügen
     this.throwInterval = setInterval(() => {
-      this.x += 10; // Geschwindigkeit der Flasche
+      this.x += 20; // Geschwindigkeit der Flasche
       if (this.x > 1000) {
         // Flasche verschwindet außerhalb des Bildschirms
         clearInterval(this.throwInterval); // Intervall stoppen
