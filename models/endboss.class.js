1b18c26605c8c08efaddec2e7e31c7aecdbf56fd
Judith L. <judith.lenz@outlook.de>
2025-01-05 14:41:39 +0100

    added hurt sound for endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -49,6 +49,8 @@ class Endboss extends MovableObject {
   ];
 
   alert_sound = new Audio("audio/rooster2.mp3");
+  hurt_sound = new Audio("audio/chicken_hurt.mp3");
+  winning_sound = new Audio("audio/arriba.mp3");
 
   constructor() {
     super().loadImage(this.IMAGES_WALKING[1]);
@@ -62,23 +64,22 @@ class Endboss extends MovableObject {
     this.x = 2500;
     this.direction = -1;
 
-    // Neue State-Variable
-    this.currentState = "idle"; // weitere States: "alert", "attack", "hurt", "dead"
-    this.currentImageIndex = 0; // für Animationen
+    this.currentState = "idle";
+    this.currentImageIndex = 0;
 
-    this.frameCounterAlert = 0; // Für Alert
-    this.frameCounterAttack = 0; // Für Attack
-    this.frameCounterIdle = 0; // Für Idle
-    this.frameCounterHurt = 0; // Für Hurt
-    this.frameCounterDead = 0; // Für Dead
-    this.frameCounterWalk = 0; // Für Walk
-    // Hitbox
+    this.frameCounterAlert = 0;
+    this.frameCounterAttack = 0;
+    this.frameCounterIdle = 0;
+    this.frameCounterHurt = 0;
+    this.frameCounterDead = 0;
+    this.frameCounterWalk = 0;
+
     this.collisionOffsetX = 40;
     this.collisionOffsetY = 70;
     this.collisionWidth = 295;
     this.collisionHeight = 360;
 
-    this.lastHit = 0; // für Treffer-Logik
+    this.lastHit = 0;
   }
 
   /**
@@ -88,15 +89,11 @@ class Endboss extends MovableObject {
   animate() {
     const minX = 2200;
     const maxX = 2600;
-
-    // Check, ob Boss tot ist (damit wir sein Dead-Verhalten regeln können)
     this.checkDeadInterval = setInterval(() => {
       if (this.isDead() && this.currentState !== "dead") {
         this.currentState = "dead";
       }
     }, 100);
-
-    // Zentrales State-Intervall
     this.stateMachineInterval = setInterval(() => {
       this.evaluateState(minX, maxX);
     }, 16);
@@ -111,23 +108,20 @@ class Endboss extends MovableObject {
       case "idle":
         this.handleIdle(minX, maxX);
         break;
-
       case "alert":
         this.handleAlert();
         break;
-
       case "attack":
         this.handleAttack();
         break;
-
       case "hurt":
         this.handleHurt();
         break;
-
       case "dead":
         this.handleDead();
         break;
-
+      case "final":
+        break;
       default:
         console.warn("Unbekannter State:", this.currentState);
     }
@@ -137,19 +131,13 @@ class Endboss extends MovableObject {
    * State: Idle (bzw. "Normalzustand")
    */
   handleIdle(minX, maxX) {
-    this.frameCounterWalk++; // Zähle bei jedem evaluateState()-Aufruf hoch
-    const FRAMES_TO_SKIP = 15; // z.B. 3 -> jedes 3. Mal Bilderwechsel
+    this.frameCounterWalk++;
+    const FRAMES_TO_SKIP = 15;
     if (this.frameCounterWalk >= FRAMES_TO_SKIP) {
       this.frameCounterWalk = 0;
-      // Zuerst die Geh-Animation
       this.playAnimation(this.IMAGES_WALKING);
-
-      // Bewegung nach links oder rechts
       this.moveBoss(minX, maxX);
-
-      // Check, ob Character nahe ist
       if (this.isCharacterClose() && !this.isDead()) {
-        // Dann gehen wir in den Alert-Zustand
         this.currentState = "alert";
         this.currentImageIndex = 0;
         this.alert_sound.play();
@@ -161,22 +149,18 @@ class Endboss extends MovableObject {
    * State: Alert
    */
   handleAlert() {
-    this.frameCounterAlert++; // Zähle bei jedem evaluateState()-Aufruf hoch
-    const FRAMES_TO_SKIP = 13; // z.B. 3 -> jedes 3. Mal Bilderwechsel
+    this.frameCounterAlert++;
+    const FRAMES_TO_SKIP = 13;
     if (this.frameCounterAlert >= FRAMES_TO_SKIP) {
       this.frameCounterAlert = 0;
-      // Alert-Animation
       if (this.currentImageIndex < this.IMAGES_ALERT.length) {
         this.img = this.imageCache[this.IMAGES_ALERT[this.currentImageIndex]];
         this.currentImageIndex++;
       } else {
-        // Fertig mit Alert-Animation -> Angriff starten
         this.currentState = "attack";
         this.currentImageIndex = 0;
       }
     }
-
-    // Falls der Character NICHT mehr nah ist, gehen wir zurück zu "idle"
     if (!this.isCharacterClose() && !this.isDead()) {
       this.currentState = "idle";
       this.currentImageIndex = 0;
@@ -187,16 +171,14 @@ class Endboss extends MovableObject {
    * State: Attack
    */
   handleAttack() {
-    this.frameCounterAttack++; // Zähle bei jedem evaluateState()-Aufruf hoch
-    const FRAMES_TO_SKIP = 12; // z.B. 3 -> jedes 3. Mal Bilderwechsel
+    this.frameCounterAttack++;
+    const FRAMES_TO_SKIP = 12;
     if (this.frameCounterAttack >= FRAMES_TO_SKIP) {
       this.frameCounterAttack = 0;
       if (this.currentImageIndex < this.IMAGES_ATTACK.length) {
         this.img = this.imageCache[this.IMAGES_ATTACK[this.currentImageIndex]];
         this.currentImageIndex++;
       } else {
-        // Angriff abgeschlossen
-        // Falls Character immer noch nah -> wieder Alert
         if (this.isCharacterClose() && !this.isDead()) {
           this.currentState = "alert";
         } else {
@@ -211,16 +193,14 @@ class Endboss extends MovableObject {
    * State: Hurt
    */
   handleHurt() {
-    this.frameCounterHurt++; // Zähle bei jedem evaluateState()-Aufruf hoch
-    const FRAMES_TO_SKIP = 11; // z.B. 3 -> jedes 3. Mal Bilderwechsel
+    this.frameCounterHurt++;
+    const FRAMES_TO_SKIP = 11;
     if (this.frameCounterHurt >= FRAMES_TO_SKIP) {
       this.frameCounterHurt = 0;
-      // Hurt-Animation
       if (this.currentImageIndex < this.IMAGES_HURT.length) {
         this.img = this.imageCache[this.IMAGES_HURT[this.currentImageIndex]];
         this.currentImageIndex++;
       } else {
-        // Nach "Hurt" wieder gucken, ob Character nah ist
         if (this.isCharacterClose() && !this.isDead()) {
           this.currentState = "alert";
         } else {
@@ -235,17 +215,16 @@ class Endboss extends MovableObject {
    * State: Dead
    */
   handleDead() {
-    this.frameCounterDead++; // Zähle bei jedem evaluateState()-Aufruf hoch
-    const FRAMES_TO_SKIP = 5; // z.B. 3 -> jedes 3. Mal Bilderwechsel
+    this.frameCounterDead++;
+    const FRAMES_TO_SKIP = 5;
     if (this.frameCounterDead >= FRAMES_TO_SKIP) {
       this.frameCounterDead = 0;
-      // Wenn Sterbeanimation noch nicht abgeschlossen ist
       if (this.currentImageIndex < this.IMAGES_DEAD.length) {
         this.img = this.imageCache[this.IMAGES_DEAD[this.currentImageIndex]];
         this.currentImageIndex++;
       } else {
-        // Wenn durch mit der Animation -> fallen lassen
         this.fallDown();
+        this.currentState = "final";
       }
     }
   }
@@ -255,7 +234,6 @@ class Endboss extends MovableObject {
    * Bewegung: von rechts nach links, bzw. Richtungswechsel an Grenzen.
    */
   moveBoss(minX, maxX) {
-    // Nur wenn Boss nicht tot und nicht hurt
     if (!this.isDead() && this.currentState !== "hurt") {
       if (this.direction === -1) {
         this.moveLeft();
@@ -264,7 +242,6 @@ class Endboss extends MovableObject {
         this.moveRight();
         this.otherDirection = true;
       }
-      // Richtung wechseln
       if (this.x <= minX) {
         this.direction = 1;
       } else if (this.x >= maxX) {
@@ -291,22 +268,18 @@ class Endboss extends MovableObject {
    */
   endbossHit() {
     if (this.currentState === "attack") {
-      console.log("Endboss ist im Angriff. Treffer ignoriert.");
       return;
     }
     console.log("Endboss getroffen!");
     console.log("EndbossEnergie vor dem Treffer:", this.energy);
-
     if (this.energy === 0) {
       console.log("Endboss ist bereits tot!");
       return;
     }
-
     const currentTime = new Date().getTime();
     if (currentTime - this.lastHit > 500) {
       this.energy -= 20;
       console.log("EndbossEnergie nach dem Treffer:", this.energy);
-
       if (this.energy <= 0) {
         this.energy = 0;
         this.currentImageIndex = 0;
@@ -314,7 +287,7 @@ class Endboss extends MovableObject {
         this.currentState = "dead";
       } else {
         this.lastHit = currentTime;
-        // State auf "hurt" wechseln
+        this.hurt_sound.play();
         this.currentState = "hurt";
         this.currentImageIndex = 0;
       }
@@ -326,10 +299,9 @@ class Endboss extends MovableObject {
    */
   fallDown() {
     if (this.hasFallen) {
-      return; // Abbruch, wenn wir schon gefallen sind
+      return;
     }
     this.hasFallen = true;
-
     let fallInterval = setInterval(() => {
       this.y += 5;
       if (this.y > 720) {
